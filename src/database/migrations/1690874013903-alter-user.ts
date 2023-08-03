09e47990cf8f203d951de3b81bd094a1770cf3fc
khanghm@zigvy.com <khanghoang1702@gmail.com>
2023-08-03 10:36:33 +0700

    add migration file

---
+++ src/database/migrations/1690874013903-alter-user.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterUser1690874013903 implements MigrationInterface {
+    name = 'AlterUser1690874013903'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_368e146b785b574f42ae9e53d5e"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_368e146b785b574f42ae9e53d5e"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_368e146b785b574f42ae9e53d5e" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_368e146b785b574f42ae9e53d5e"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_368e146b785b574f42ae9e53d5e" UNIQUE ("roleId")`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_368e146b785b574f42ae9e53d5e" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
